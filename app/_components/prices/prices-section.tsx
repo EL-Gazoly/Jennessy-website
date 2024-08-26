230389bbdc8eac8220068a25be4721af9b0e3505
El-Gazoly <elgazoly11@gmail.com>
2024-08-26 18:49:36 +0300

    Feature : Add pricing section

--- app/_components/prices/prices-section.tsx
+++ app/_components/prices/prices-section.tsx
@@ -2,14 +2,14 @@ import { Badge } from "@/components/ui/badge";
 import PricePlanCard from "./price-plan-card";
 const PricesSection = () => {
   return (
-    <div className="p-4 lg:px-10 xl:px-20 flex flex-col gap-y-4">
+    <div className="p-4 lg:px-10 xl:px-20 flex flex-col gap-y-4 mt-20">
       <Badge className=" rounded-lg text-base font-normal lg:text-lg">
         Find the Perfect Plan for Your Journey
       </Badge>
       <h2 className=" text-white text-3xl font-extrabold xl:text-5xl">
         Pricing:
       </h2>
-      <div className=" mt-9 lg:mt-12 flex flex-wrap  gap-5 lg:gap-8 xl:gap-10 justify-center md:justify-start xl:justify-around ">
+      <div className=" mt-9 lg:mt-12 flex flex-wrap  gap-5 lg:gap-8 xl:gap-10 justify-center lg:justify-start xl:justify-around ">
         <PricePlanCard
           name="Essential"
           price="$1050"
