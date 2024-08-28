8dc27e1ded1a7a7c4f830f0a28fc77e03485a5a4
El-Gazoly <elgazoly11@gmail.com>
2024-08-28 22:04:41 +0300

    Feature : Add into view animation to contact us and why choose us sections

--- app/_components/contact/contact-us-section.tsx
+++ app/_components/contact/contact-us-section.tsx
@@ -4,31 +4,73 @@ import CalenderyLarage from "@/public/calendery-pc.svg";
 import LinearPattern from "@/public/linear-pattern.svg";
 import Image from "next/image";
 import { InlineWidget } from "react-calendly";
+import { motion } from "framer-motion";
+import { useInView } from "@/hooks/use-in-view";
+import TypingEffect from "@/hooks/use-typing-effect";
+import { useState } from "react";
+
 const ContactUsSection = () => {
+  const { ref, isInView } = useInView();
+  const [isTypingComplete, setIsTypingComplete] = useState(false);
+
+  const handleTypingComplete = () => {
+    setIsTypingComplete(true);
+  };
+
+  const pVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 0.3,
+        duration: 0.7,
+        ease: "easeOut",
+      },
+    },
+  };
+
   return (
-    <div className=" mt-14 flex flex-col gap-y-4  p-4 lg:px-10 xl:px-20 ">
-      <Badge className=" rounded-lg text-base font-normal lg:text-lg">
+    <div
+      className="mt-14 flex flex-col gap-y-4 p-4 lg:px-10 xl:px-20"
+      ref={ref}
+    >
+      <Badge className="rounded-lg text-base font-normal lg:text-lg">
         Get in Touch
       </Badge>
-      <h2 className=" text-white text-3xl font-extrabold xl:text-5xl">
-        Contact
+      <h2 className="text-white text-3xl font-extrabold xl:text-5xl">
+        {isInView && (
+          <TypingEffect
+            text="  Contact Us:"
+            speed={100}
+            onComplete={handleTypingComplete}
+            isTypingComplete={isTypingComplete}
+          />
+        )}
       </h2>
-      <p className="text-white text-xl font-semibold leading-[125%] mt-2.5 lg:mt-5 lg:text-3xl">
-        <span className="text-[26px] text-[#007AFF] lg:text-4xl">D</span>o you
-        need to pull a list of motivated sellers? Ready to take your real estate
-        business to the next level? Contact us today to learn how Jennesy can
-        help you generate high-quality leads and grow your client base. Let’s
-        start a conversation
-      </p>
-      <div className=" relative flex w-full h-full items-center justify-center  mt-12 md:mt-[77px] lg:mt-[107px]">
-        <div className=" absolute inset-0 self-center justify-self-center">
+      {isTypingComplete && (
+        <motion.p
+          className="text-white text-xl font-semibold leading-[125%] mt-2.5 lg:mt-5 lg:text-3xl"
+          initial="hidden"
+          animate={isTypingComplete && isInView ? "visible" : "hidden"}
+          variants={pVariants}
+        >
+          <span className="text-[26px] text-[#007AFF] lg:text-4xl">D</span>o you
+          need to pull a list of motivated sellers? Ready to take your real
+          estate business to the next level? Contact us today to learn how
+          Jennesy can help you generate high-quality leads and grow your client
+          base. Let’s start a conversation
+        </motion.p>
+      )}
+      <div className="relative flex w-full h-full items-center justify-center mt-12 md:mt-[77px] lg:mt-[107px]">
+        <div className="absolute inset-0 self-center justify-self-center">
           <Image
             src={LinearPattern}
             alt="LinearPattern"
             className="object-cover"
           />
         </div>
-        <div className=" z-10  max-w-[895px] max-h-[800px] self-center w-full h-full">
+        <div className="z-10 max-w-[895px] max-h-[800px] self-center w-full h-full">
           <InlineWidget url="https://calendly.com/jennessy-support/30min" />
         </div>
       </div>
--- app/_components/why-chosse-us.tsx
+++ app/_components/why-chosse-us.tsx
@@ -1,10 +1,33 @@
 import { Badge } from "@/components/ui/badge";
 import Pattern from "@/public/pattern.svg";
 import Image from "next/image";
+import TypingEffect from "@/hooks/use-typing-effect";
+import { useInView } from "@/hooks/use-in-view";
+import { useState } from "react";
+import { motion } from "framer-motion";
 
 const WhyChooseUs = () => {
+  const { ref, isInView } = useInView();
+  const [isTypingComplete, setIsTypingComplete] = useState(false);
+
+  const handleTypingComplete = () => {
+    setIsTypingComplete(true);
+  };
+  const pVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 0.3,
+        duration: 0.7,
+        ease: "easeOut",
+      },
+    },
+  };
+
   return (
-    <div className="relative mt-14 md:mt-28">
+    <div className="relative mt-14 md:mt-28" ref={ref}>
       <div className="absolute inset-0 opacity-20">
         <Image src={Pattern} alt="Pattern" layout="fill" objectFit="cover" />
       </div>
@@ -13,14 +36,26 @@ const WhyChooseUs = () => {
           Experience the Jennesy advantage today.
         </Badge>
         <h2 className="text-white text-3xl font-extrabold xl:text-5xl">
-          Why Choose Us:
+          {isInView && (
+            <TypingEffect
+              text="  Why Choose Us:"
+              speed={100}
+              isTypingComplete={isTypingComplete}
+              onComplete={handleTypingComplete}
+            />
+          )}
         </h2>
-        <p className="text-white text-xl font-semibold leading-[125%] mt-2 lg:mt-5 lg:text-3xl">
+        <motion.p
+          className="text-white text-xl font-semibold leading-[125%] mt-2 lg:mt-5 lg:text-3xl"
+          initial="hidden"
+          animate={isTypingComplete && isInView ? "visible" : "hidden"}
+          variants={pVariants}
+        >
           <span className="text-[26px] text-[#007AFF] lg:text-4xl">A</span>re
           you a real estate professional striving for excellence? Partner with
           us for support in wholesaling, fixing and flipping, and property
           acquisitions, elevating your business to new heights.
-        </p>
+        </motion.p>
       </div>
     </div>
   );
