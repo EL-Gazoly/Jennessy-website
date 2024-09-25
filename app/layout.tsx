8003681619be469106174eb122fbbc31d4b8a532
El-Gazoly <elgazoly11@gmail.com>
2024-09-25 19:58:03 +0300

    feature: add thumbnail

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,12 +5,50 @@ import { ThemeProvider } from "@/components/theme-provider";
 import Head from "next/head";
 import { cn } from "@/lib/utils";
 import { NextUIProviders } from "@/providers/nextjs-provider";
+import Thumbnail from "@/public/Thumbnail.jpeg";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Jennesy",
-  description: "Jennsey is a real estate contact center.",
+  description:
+    "turn is teaming up with Cookies and Lemonnade storefronts to dole out custom longboards in our go long!(board) giveaway. Every location has one (1) completely unique design, and with no limit to how many you can win… can you collect them all?!",
+  metadataBase: new URL("https://jennessy.net"),
+  openGraph: {
+    title: `Jennesy`,
+    description: "Jennsey is a real estate contact center.",
+    url: "https://jennessy.net",
+    siteName: "turn",
+    images: [
+      {
+        url: Thumbnail.src,
+        width: 200,
+        height: 200,
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Jennesy",
+    description: "Jennsey is a real estate contact center.",
+
+    images: [Thumbnail.src],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
   icons: {
     icon: ["/favicon.ico?v=4"],
     apple: ["/apple-touch-icon.png?v=4"],
